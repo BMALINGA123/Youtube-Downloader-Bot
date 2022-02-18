9ff8484a972c825e44d4c16fa8a08307c969ae2f
BMALINGA123 <96827461+BMALINGA123@users.noreply.github.com>
2022-02-18 20:45:57 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,10 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/BioHazard_bots")],
+        [InlineKeyboardButton("🍀 zoneunlimited 🍀 ", url="https://t.me/zoneunlimited")],
         [InlineKeyboardButton(
-            "Report Bugs", url="https://t.me/BHZBot_Support")]
+            "🍀 zoneunlimited chat 🍀", url="https://t.me/zoneunlimitedchat")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"🍀 Hello There{message.from_user.first_name}\n\n🙋‍♂️ I am YOUTUBE DOWNLOADER BOT 🎥,I can\n\n🎵 Downloading All song\n🌷 Downloading All Video\n\n⭕️ Inline search\n🌺 Group Supported\n🎯 24 horse active\n\n🔥 Bot Commands 🔥\n/song\n/video\n\n🌿 Developer : @chamod_deshan\n\n🔥 [🍀  zoneunlimited  🍀](https://t.me/zoneunlimited) Corporation ©️"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
